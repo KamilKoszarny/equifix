eb1b2dbb42a0e66c2385466c496a2c15523aa0ba
Kamil Koszarny <kamil.koszarny@pentacomp.pl>
2019-09-10 08:30:35 +0200

    initial spring boot import

---
+++ src/main/java/com/kamilkoszarny/equifix/equifix/EquifixApplication.java
@@ -0,0 +1,13 @@
+package com.kamilkoszarny.equifix.equifix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EquifixApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EquifixApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kamilkoszarny/equifix/equifix/EquifixApplicationTests.java
@@ -0,0 +1,16 @@
+package com.kamilkoszarny.equifix.equifix;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class EquifixApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
