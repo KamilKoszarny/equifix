aaafd507786ffbe8435026256964a5a3f8b61781
Kamil Koszarny <kamil.koszarny@pentacomp.pl>
2019-09-24 21:21:06 +0200

    configure angular - naming fix

--- equifix-server/src/main/java/com/kamilkoszarny/equifix/EquifixApplication.java
+++ equifix-server/src/main/java/com/kamilkoszarny/equifix/EquifixApplication.java
@@ -2,16 +2,10 @@ package com.kamilkoszarny.equifix;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
 public class EquifixApplication extends SpringBootServletInitializer {
-
-	@Override
-	protected SpringApplicationBuilder configure(SpringApplicationBuilder applicationBuilder) {
-		return applicationBuilder.sources(EquifixApplication.class);
-	}
 
 	public static void main(String[] args) {
 		disableIllegalReflectiveAccessWarning();
