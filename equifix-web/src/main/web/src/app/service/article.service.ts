5f95efef046a13719b75f0a7d4fa447c1eb5870c
Kamil Koszarny <kamil.koszarny@pentacomp.pl>
2019-10-05 10:25:04 +0200

    angular: article model, service and other files

---
+++ equifix-web/src/main/web/src/app/service/article.service.ts
@@ -0,0 +1,18 @@
+import {Injectable} from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {Article} from '../model/article';
+import {Observable} from "rxjs";
+
+@Injectable()
+export class ArticleService {
+
+  private readonly articlesUrl: string;
+
+  constructor(private http: HttpClient) {
+    this.articlesUrl = 'http://localhost:8092/equifixserver_war/articles';
+  }
+
+  public findAll(): Observable<Article[]> {
+    return this.http.get<Article[]>(this.articlesUrl);
+  }
+}
