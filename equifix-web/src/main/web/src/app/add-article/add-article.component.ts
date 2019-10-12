08282a9102cdeaf6616528b605352c85f032fa59
Kamil Koszarny <kamil.koszarny@pentacomp.pl>
2019-10-12 14:25:47 +0200

    add article: category

--- equifix-web/src/main/web/src/app/add-article/add-article.component.ts
+++ equifix-web/src/main/web/src/app/add-article/add-article.component.ts
@@ -1,19 +1,26 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {Article} from "../model/article";
 import {ArticleService} from "../service/article.service";
 import {Router} from "@angular/router";
+import {Category} from "../model/category";
+import {CategoryService} from "../service/category.service";
 
 @Component({
   selector: 'app-add-article',
   templateUrl: './add-article.component.html',
   styleUrls: ['./add-article.component.css']
 })
-export class AddArticleComponent {
+export class AddArticleComponent implements OnInit{
 
   article: Article;
+  categories: Category[];
 
-  constructor(private articleService: ArticleService, private router: Router) {
+  constructor(private articleService: ArticleService, private categoryService: CategoryService, private router: Router) {
     this.article = new Article();
+  }
+
+  ngOnInit(): void {
+    this.categoryService.findAll().subscribe(result => {this.categories = result});
   }
 
   onSubmit() {
